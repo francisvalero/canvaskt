100f2bb9266543dd63f48a8682a5dfa9a58863e3
francis <francisvalero@live.com>
2016-09-26 11:58:52 +0400

    #

--- src/core/Animation.kt
+++ src/core/Animation.kt
@@ -3,6 +3,7 @@ package core
 import javafx.animation.AnimationTimer
 import javafx.scene.canvas.Canvas
 import javafx.scene.canvas.GraphicsContext
+import javafx.scene.input.MouseEvent.*
 
 
 class AnimationCanvas( val draw: (g: GraphicsContext, e: Environment) -> Unit,
@@ -11,10 +12,24 @@ class AnimationCanvas( val draw: (g: GraphicsContext, e: Environment) -> Unit,
     init {
         width = w
         height = h
+        addEventFilter(ANY){
+            when(it.eventType){
+                MOUSE_MOVED, MOUSE_DRAGGED -> {
+                    mouseX = it.x
+                    mouseY = it.y
+                }
+                MOUSE_PRESSED -> isMousePressed = true
+                MOUSE_RELEASED -> isMousePressed = false
+            }
+        }
     }
+    var mouseX: Double = 0.0
+    var mouseY: Double = 0.0
+    var isMousePressed: Boolean = false
+
     val timer = object : AnimationTimer() {
         override fun handle(now: Long){
-            draw(graphicsContext2D, Environment(now))
+            draw(graphicsContext2D, Environment(now, mouseX, mouseY, isMousePressed))
         }
     }
     override fun render() = timer.start()
@@ -26,10 +41,10 @@ interface Renderable{
     fun render()
 }
 
-class Environment(val now:Long)
+class Environment(val now:Long, val mouseX: Double, val mouseY: Double, val isMousePressed: Boolean)
 
 fun graphics(f: GraphicsContext.(e: Environment) -> Unit) : (g: GraphicsContext, e: Environment) -> Unit{
     return { g:GraphicsContext, e: Environment -> g.f(e) }
 }
 
-fun defaultEnvironment() = Environment(0.toLong())
+fun defaultEnvironment() = Environment(0.toLong(), 0.0, 0.0, false)
