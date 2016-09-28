49db4f93ba939f32e0783760d77a1cd63fb854b5
francis <francisvalero@live.com>
2016-09-28 19:37:05 +0400

    Vector

---
+++ src/core/Vector2D.kt
@@ -0,0 +1,15 @@
+package core
+
+import java.lang.Math.*;
+
+data class Vector2D(val x: Double, val y: Double) {
+    operator fun plus(v: Vector2D) = Vector2D(x + v.x, y + v.y)
+    operator fun minus(v: Vector2D) = Vector2D(x + v.x, y - v.y)
+    operator fun times(n: Double) = Vector2D(x * n, y * n)
+    operator fun div(n: Double) = Vector2D(x / n, y / n)
+}
+
+fun main(args: Array<String>) {
+
+}
+
